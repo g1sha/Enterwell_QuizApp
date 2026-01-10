96235c5c8da7889d9d5d3b4c923e618fa4338529
Emir Ibrisimovic <ibrisimovice@outlook.com>
2026-01-10 22:58:45 +0100

    Added comments to code

--- API/Controllers/QuestionsController.cs
+++ API/Controllers/QuestionsController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Constants;
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Constants;
 using Core.DTOs;
 using Core.DTOs.Pagination;
 using Core.DTOs.Question;
@@ -12,13 +13,25 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class QuestionsController(IQuestionService questionService) : ControllerBase
 {
+    /// <summary>
+    /// Get paginated list of questions with optional filtering on question text
+    /// </summary>
+    /// <param name="pageNumber"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="filter"></param>
+    /// <returns></returns>
     [HttpGet]
-    public async Task<ActionResult<PaginatedResponseDto<QuestionDto>>> GetQuestions([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10, [FromQuery] string? filter = null)
+    public async Task<ActionResult<PaginatedResponseDto<QuestionDto>>> GetQuestions([FromQuery][Range(1,int.MaxValue)] int pageNumber = 1, [FromQuery][Range(1,100)] int pageSize = 10, [FromQuery] string? filter = null)
     {
         var questions = await questionService.GetAllQuestionsAsync(pageNumber, pageSize, filter);
         return Ok(questions);
     }
     
+    /// <summary>
+    /// Get a specific question by its ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpGet("{id:int}")]
     public async Task<ActionResult<QuestionDto>> GetQuestion(int id)
     {
@@ -29,6 +42,11 @@ public class QuestionsController(IQuestionService questionService) : ControllerB
         return Ok(question);
     }
 
+    /// <summary>
+    /// Create a new question
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<ActionResult<QuestionDto>> CreateQuestion([FromBody] CreateQuestionDto dto)
     {
@@ -36,10 +54,18 @@ public class QuestionsController(IQuestionService questionService) : ControllerB
         return CreatedAtAction(nameof(GetQuestion), new { id = createdQuestion.Id }, createdQuestion);
     }
 
+    /// <summary>
+    /// Update an existing question by its ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dto"></param>
+    /// <returns></returns>
     [HttpPut("{id:int}")]
     public async Task<ActionResult> UpdateQuestion(int id, [FromBody] UpdateQuestionDto dto)
     {
-        await questionService.UpdateQuestionAsync(id, dto);
+        var updatedQuestion=await questionService.UpdateQuestionAsync(id, dto);
+        if (updatedQuestion == null)
+            return NotFound(ErrorResponse.Create(ErrorMessages.QuestionNotFound));
         return NoContent();
     }
 }
