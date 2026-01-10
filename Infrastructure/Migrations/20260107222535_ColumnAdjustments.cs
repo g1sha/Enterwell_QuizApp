dd3f779445ed1d8c820a252087695d11c5dd21e1
Emir Ibrisimovic <ibrisimovice@outlook.com>
2026-01-10 22:56:55 +0100

    Added changes/creation tracking columns and missing migrations

---
+++ Infrastructure/Migrations/20260107222535_ColumnAdjustments.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnAdjustments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Answer",
+                table: "Questions",
+                newName: "CorrectAnswer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CorrectAnswer",
+                table: "Questions",
+                newName: "Answer");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20260108150423_Removed position column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removedpositioncolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "QuizQuestions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "QuizQuestions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
