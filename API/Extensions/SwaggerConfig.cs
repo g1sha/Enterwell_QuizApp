4abebb6209ffa4cfd4d74683683bf98828edb2d7
Emir Ibrisimovic <ibrisimovice@outlook.com>
2026-01-11 15:25:36 +0100

    Cleanup Program.cs and postman update

---
+++ API/Extensions/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace API.Extensions;
+
+public static class SwaggerConfig
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(swgr =>
+        {
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            swgr.IncludeXmlComments(xmlPath);
+
+            swgr.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter your JWT token"
+            });
+
+            swgr.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using API.Extensions;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -14,43 +15,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSwaggerGen( swgr=>
-{
-    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    swgr.IncludeXmlComments(xmlPath);
-});
 
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddIdentityCore<User>(options =>
-    {
-        options.Password.RequiredLength = 6;
-        options.Password.RequireUppercase = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireDigit = false;
-    })
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<QuizContext>();
+// Swagger from Extensions
+builder.Services.AddSwaggerDocumentation();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime =  true,
-        ValidateIssuerSigningKey =  true,
-        ValidIssuer =  builder.Configuration.GetSection("Jwt:Issuer").Value,
-        ValidAudience =   builder.Configuration.GetSection("Jwt:Audience").Value,
-        IssuerSigningKey =  new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value))
-    };
-});
-    
+// Identity and JWT from Extensions
+builder.Services.AddIdentityServices(builder.Configuration);
+
 builder.Services.AddDbContext<QuizContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
